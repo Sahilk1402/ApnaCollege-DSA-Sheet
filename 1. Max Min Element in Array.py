2b2c0812b348c9314388a7b1a6f1e0850ba52e0d
Sahilk1402 <81619744+Sahilk1402@users.noreply.github.com>
2023-06-17 23:11:23 +0530

    Create 1. Max Min Element in Array.py

---
+++ 1. Max Min Element in Array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findSum(self,A,N): 
+        #code here
+        minA = min(A)
+        maxA = max(A)
+        sum = minA + maxA
+        return sum
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+
+t=int(input())
+for _ in range(0,t):
+    n=int(input())
+    a=list(map(int,input().split()))
+    ob = Solution()
+    print(ob.findSum(a,n))
+# } Driver Code Ends
